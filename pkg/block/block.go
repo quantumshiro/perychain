4459c0446dd3a955706641d698fdb3cccf99860a
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 01:15:08 +0900

    add: toString method to Block struct

--- pkg/block/block.go
+++ pkg/block/block.go
@@ -10,3 +10,11 @@ type Block struct {
 func NewBlock(timestamp int64, lastHash []byte, hash []byte, data []byte) *Block {
 	return &Block{timestamp, lastHash, hash, data}
 }
+
+func (b *Block) toString() string {
+	return "Block -\n" +
+		"Timestamp: " + string(b.timestamp) + "\n" +
+		"Last Hash: " + string(b.lastHash) + "\n" +
+		"Hash: " + string(b.hash) + "\n" +
+		"Data: " + string(b.data) + "\n"
+}
