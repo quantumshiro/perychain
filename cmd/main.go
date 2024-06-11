66ff22fdce5e6b4045353d99a0ad6b59df4e10d0
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 14:02:22 +0900

    add: mining endpoint and show blocks endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+
+	"encoding/json"
+
+	"github.com/quantumshiro/perychain/pkg/blockchain"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func blocks(w http.ResponseWriter, r *http.Request) {
+	bc := blockchain.NewBlockChain()
+
+	json.NewEncoder(w).Encode(bc.Chain)
+}
+
+func mine(w http.ResponseWriter, r *http.Request) {
+	bc := blockchain.NewBlockChain()
+	// data: request body data
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	bc.AddBlock(data)
+
+	json.NewEncoder(w).Encode(bc.Chain)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/blocks", blocks)
+	http.HandleFunc("/mine", mine)
 	http.ListenAndServe(":8080", nil)
 }
