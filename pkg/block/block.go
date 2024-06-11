4d3ba8910f4cf448da1fe84aa5800934cb12e947
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 09:05:22 +0900

    ref: rename tostring function

--- pkg/block/block.go
+++ pkg/block/block.go
@@ -11,9 +11,9 @@ func NewBlock(timestamp int64, lastHash []byte, hash []byte, data []byte) *Block
 	return &Block{timestamp, lastHash, hash, data}
 }
 
-func (b *Block) toString() string {
+func (b *Block) String() string {
 	return "Block -\n" +
-		"Timestamp: " + string(b.timestamp) + "\n" +
+		"Timestamp: " + string(rune(b.timestamp)) + "\n" +
 		"Last Hash: " + string(b.lastHash) + "\n" +
 		"Hash: " + string(b.hash) + "\n" +
 		"Data: " + string(b.data) + "\n"
