6b764f68865fa80515d604142d10344e3330ad44
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 13:09:43 +0900

    add: chain validation function to BlockChain

--- pkg/blockchain/blockchain.go
+++ pkg/blockchain/blockchain.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"reflect"
+
 	"github.com/quantumshiro/perychain/pkg/block"
 )
 
@@ -12,4 +14,22 @@ func (bc *BlockChain) AddBlock(data []byte) {
 	lastBlock := bc.Chain[len(bc.Chain)-1]
 	newBlock := block.MineBlock(lastBlock, data)
 	bc.Chain = append(bc.Chain, newBlock)
+}
+
+// validate the chain
+func (bc *BlockChain) Validate(chain *BlockChain) bool {
+	if chain.Chain[0].String() != block.Genesis().String() {
+		return false
+	}
+
+	for i := 1; i < len(chain.Chain); i++ {
+		blocks := chain.Chain[i]
+		lastBlocks := chain.Chain[i-1]
+
+		if !reflect.DeepEqual(blocks.LastHash, lastBlocks.Hash) || !reflect.DeepEqual(blocks.Hash, blocks.BlockHash()) {
+			return false
+		}
+	}
+
+	return true
 }
