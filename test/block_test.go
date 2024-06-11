f245568e0f9f00429f02563481833a4a3dd11bad
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 10:09:34 +0900

    add: test for MineBlock function

--- test/block_test.go
+++ test/block_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/quantumshiro/perychain/pkg/block"
@@ -20,5 +21,18 @@ func TestGenesis(t *testing.T) {
 
 	if b.String() != block.NewBlock(1, []byte{}, []byte{}, []byte{}).String() {
 		t.Errorf("Block.Genesis() = %s, want %s", b.String(), block.NewBlock(1, []byte{}, []byte{}, []byte{}).String())
+	}
+}
+
+func TestMineBlock(t *testing.T) {
+	b := block.NewBlock(1, []byte("foo"), []byte("bar"), []byte("baz"))
+	newBlock := block.MineBlock(b, []byte("qux"))
+
+	if !reflect.DeepEqual(newBlock.LastHash, b.Hash) {
+		t.Errorf("MineBlock().LastHash = %s, want %s", newBlock.LastHash, b.Hash)
+	}
+
+	if !reflect.DeepEqual(newBlock.Data, []byte("qux")) {
+		t.Errorf("MineBlock().Data = %s, want %s", newBlock.Data, []byte("qux"))
 	}
 }
