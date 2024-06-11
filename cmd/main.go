e88add857e0da4dad6ec438a4a81aacd9e671546
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-11 13:31:30 +0900

    add: http initial server

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
