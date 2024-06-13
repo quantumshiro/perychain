e82a27975cb24d3541713f68e53fbf2a99107b1a
quantumshiro <hiro.1nakanishi@gmail.com>
2024-06-13 11:23:30 +0900

    fix: p2p libs

--- cmd/p2p/p2p.go
+++ cmd/p2p/p2p.go
@@ -1,8 +1,6 @@
 package p2p
 
 import (
-	"net/http"
-
 	"golang.org/x/net/websocket"
 
 	"github.com/quantumshiro/perychain/pkg/blockchain"
@@ -17,37 +15,23 @@ func NewP2P(bc *blockchain.BlockChain) *P2P {
 	return &P2P{bc, []*websocket.Conn{}}
 }
 
-func (p2p *P2P) Blocks(ws *websocket.Conn) {
+func listen(ws *websocket.Conn, p2p *P2P) {
 	p2p.sockets = append(p2p.sockets, ws)
-
-	// send the chain to the new peer
-	websocket.JSON.Send(ws, p2p.BlockChain.Chain)
 }
 
-func (p2p *P2P) Mine(data []byte) {
-	p2p.BlockChain.AddBlock(data)
-
-	// broadcast the new chain to all peers
+func connectPeers(p2p *P2P) {
 	for _, ws := range p2p.sockets {
-		websocket.JSON.Send(ws, p2p.BlockChain.Chain)
+		for _, peer := range p2p.sockets {
+			if ws == peer {
+				continue
+			}
+
+			websocket.Message.Send(ws, "Hello, World!")
+		}
 	}
 }
 
-func (p2p *P2P) Root(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
-}
-
-func (p2p *P2P) Start() {
-	http.Handle("/blocks", websocket.Handler(p2p.Blocks))
-	http.HandleFunc("/mine", func(w http.ResponseWriter, r *http.Request) {
-		data := r.URL.Query().Get("data")
-		p2p.Mine([]byte(data))
-		w.Write([]byte("Block mined!"))
-	})
-	http.HandleFunc("/", p2p.Root)
-	http.ListenAndServe(":8080", nil)
-}
-
-func (p2p *P2P) Stop() {
-	http.DefaultServeMux = http.NewServeMux()
+func ConnectSocket(ws *websocket.Conn, p2p *P2P) {
+	listen(ws, p2p)
+	connectPeers(p2p)
 }
